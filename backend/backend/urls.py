dd0e12cea8d213261a121bb27b34f6697b90b006
likhit2804 <likhit.grandhe2804@gmail.com>
2026-01-04 12:32:06 +0530

    Fix React assets serving

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,14 +3,19 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+import os
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("accounts.urls")),
+    # Serve React assets
+    re_path(r'^assets/(?P<path>.*)$', serve, {'document_root': os.path.join(settings.BASE_DIR, '..', 'dist', 'assets')}),
     # Serve React app for all other routes
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='react_app'),
 ]
 
 # Serve static files in development
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
